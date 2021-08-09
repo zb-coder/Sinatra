fbab67b6e72cd00fb982a1208ea05e88e0974622
Ahsan <rasul.ahsan@gmail.com>
2021-08-09 17:19:13 +0100

    Getting Sinatra to return a picture of a kitten with a border

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,25 @@
 require 'sinatra'
+require 'sinatra/reloader' if development?
 
 get '/' do
-  "Hello World"
+  "Hello"
 end
 
-delete '/' do
-  "World"
+get '/secret' do
+  "Shhhh it's a secret"
+end
+
+get '/makers' do
+  "Im so tired :)"
+end
+
+get '/coffee' do
+  "Im buzzing"
+end
+
+get '/cat' do
+  '<img style = "border: dashed red;"
+   src = "https://i.imgur.com/jFaSxym.png"
+   alt="Photo of cute kitten"
+  />'
 end
